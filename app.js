07777c07a2f8250c5e9bcd6a59ad402023f9d345
ymnnymnn <yaminnyinyi12@gmail.com>
2020-10-08 15:47:35 +0630

    ed

--- app.js
+++ app.js
@@ -314,7 +314,7 @@ app.get('/register/:sender_id',function(req,res){
 });
 
 
-app.post('/register',function(req,res){
+app.post('/register/:sender_id',function(req,res){
        
       let name  = req.body.name;
       let email = req.body.email;
