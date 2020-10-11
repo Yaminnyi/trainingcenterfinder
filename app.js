5b5e084c68a58895f90b0ad1a89b0902d90cf08e
ymnnymnn <yaminnyinyi12@gmail.com>
2020-10-12 05:00:52 +0630

    dds

--- app.js
+++ app.js
@@ -218,7 +218,7 @@ app.post('/admin/updateappointment', function(req,res){
       res.redirect('/admin/appointments');
   }).catch((err)=>console.log('ERROR:', error)); 
  
-});*/
+});
 
 /*********************************************
 Gallery page
