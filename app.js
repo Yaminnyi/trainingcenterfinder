ac00a3e4ebceaf361c11d36403b4a5c24ec9fc80
Yamin Nyi Nyi <yaminnyinyi12@gmail.com>
2020-10-19 17:35:33 +0630

    sfsd

--- app.js
+++ app.js
@@ -322,7 +322,7 @@ app.get('/register/:sender_id',function(req,res){
 
 app.post('/register',function(req,res){
       
-      let ref = generateRandom(6);
+      let ref = generateRandom(8);
     
       let name  = req.body.name;
       let email = req.body.email;
@@ -332,11 +332,11 @@ app.post('/register',function(req,res){
 
       console.log("AA");
       
-      db.collection('register').add({
+      db.collection('register').doc(ref).set({
       name: name,
       email: email,
       phone: phone,
-      ref: ref
+      
     }).then(success => {   
           console.log("DATA SAVED")
     let text = "Thank you for your register.Please click already registered." + "\u000A";
