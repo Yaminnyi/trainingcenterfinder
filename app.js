7952c07263f41f4f66e59de064e3a1bfdf2c5b35
Yamin Nyi Nyi <yaminnyinyi12@gmail.com>
2020-10-20 15:42:53 +0630

    ds

--- app.js
+++ app.js
@@ -363,44 +363,38 @@ app.get('/course_registration/:sender_id',function(req,res){
     const sender_id = req.params.sender_id;
     res.render('course_registration.ejs',{title:"Add courses", sender_id:sender_id});
 });
-
 app.post('/course_registration',function(req,res){
-      
+
       
       let ref = generateRandom(8);
       let name  = req.body.name;
       let email    = req.body.email;
       let phone  = req.body.phone;
       let dob = req.body.dob;
+
       
-     
       let today = new Date();
       let created_on = today;
-
       console.log("DD");
-      
       db.collection('course_registration').doc(ref).set({
       name: name,
       email:email,
       phone: phone,
       dob: dob,
-      created_on: created_on
-         
+      created_on: created_on,
+      ref:ref
     }).then(success => {   
           console.log("DATA SAVED")
-    let text = "Your reference id is" + ref + "\u000A";
+    let text = "Thank you for your register. Your data has been saved. If you leave your message, you write cancel. Your reference id is " + data.ref;
         
-    let response = {
-      "text": text
-    };
+    let response = {"text": text};
     callSend(user_id,response);
     }).catch(error => {
           console.log(error);
       }); 
-     
-         
-});
 
+     
+});
 
 //route url
 
