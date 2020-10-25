da164bdd3c4300ce04ebe1569ac93bb975e8504a
Yamin Nyi Nyi <yaminnyinyi12@gmail.com>
2020-10-25 20:08:48 +0630

    fsdf

--- app.js
+++ app.js
@@ -373,7 +373,7 @@ app.post('/give_review',function(req,res){
       let ref = generateRandom(8);
     
       let name  = req.body.name;
-      let id = req.body.id;
+      
       let tc = req.body.tc;
       let course = req.body.course;
       let review = req.body.review;
@@ -385,7 +385,7 @@ app.post('/give_review',function(req,res){
       db.collection('give_review').add({
       
       name: name,
-      id:id,
+     
       tc: tc,
       course: course,
       review: review
