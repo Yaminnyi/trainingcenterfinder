e426fe333f341b84213eef7d05af8d5da4a504e8
Yamin Nyi Nyi <yaminnyinyi12@gmail.com>
2020-10-30 18:20:46 +0630

    dfds

--- app.js
+++ app.js
@@ -797,7 +797,21 @@ let data = [];
 });
 
 
-
+var list=[‘one’,’two’,’three’,’four’,’five’];
+list.forEach(function(element) {
+document.getElementById(element).addEventListener(“click”, function(){
+var cls=document.getElementById(element).className;
+if(cls.includes(“unchecked”))
+{
+document.getElementById(element).classList.remove(“unchecked”);
+document.getElementById(element).classList.add(“checked”);
+}
+else
+{
+document.getElementById(element).classList.remove(“checked”); document.getElementById(element).classList.add(“unchecked”);
+}
+});
+});
 
 
 app.post('/show', function(req, res){
