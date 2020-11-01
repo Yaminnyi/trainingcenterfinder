2d8fec53e4cc8b696e2c12df542d1aa44fa9714f
Yamin Nyi Nyi <yaminnyinyi12@gmail.com>
2020-11-01 07:35:40 +0630

    dfdfd

--- app.js
+++ app.js
@@ -797,27 +797,6 @@ let data = [];
 });
 
 
-app.get('/course_registration', async function(req,res){
-
-var list=[‘one’,’two’,’three’,’four’,’five’];
-list.forEach(function(element) {
-document.getElementById(element).addEventListener(“click”, function(){
-var cls=document.getElementById(element).className;
-if(cls.includes(“unchecked”))
-{
-document.getElementById(element).classList.remove(“unchecked”);
-document.getElementById(element).classList.add(“checked”);
-}
-else
-{
-document.getElementById(element).classList.remove(“checked”); document.getElementById(element).classList.add(“unchecked”);
-}
-});
-});
-
-}
-
-
 
 
 
